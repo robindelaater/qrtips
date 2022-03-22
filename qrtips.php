136acd93834ceacb83286b01e78bac398f23f6de
Robin de Laater <rdelaater@icloud.com>
2022-03-22 09:52:13 +0100

    Require MultiSafepay plugin

--- qrtips.php
+++ qrtips.php
@@ -8,9 +8,44 @@
  * Author URI: https://delaater.nl/
  */
 
-add_shortcode('qr_tips', 'qr_tips_output');
+use QRTip\WooCommerce\QRTip;
 
-function qr_tips_output(): string
+add_action('admin_init', 'child_plugin_has_parent_plugin');
+add_action('after_setup_theme', 'setup', 5);
+
+add_shortcode('qr_tips', 'qrTipsOutput');
+
+function setup()
 {
-  return "<h1>Hello, world!</h1>";
+  require plugin_dir_path(__FILE__) . 'src/QRTip.php';
+}
+
+/**
+ * Require MultiSafepay plugin to be installed.
+ * @return void
+ */
+function child_plugin_has_parent_plugin()
+{
+  if (is_admin() && current_user_can('activate_plugins') && !is_plugin_active('multisafepay/multisafepay.php')) {
+    add_action('admin_notices', 'child_plugin_notice');
+
+    deactivate_plugins(plugin_basename(__FILE__));
+
+    if (isset($_GET['activate'])) {
+      unset($_GET['activate']);
+    }
+  }
+}
+
+function child_plugin_notice()
+{
+  ?>
+  <div class="error"><p>Sorry, but the QR Tips plugin requires the MultiSafepay Payments plugin to be installed and
+      active.</p></div><?php
+}
+
+function qrTipsOutput(): string
+{
+  $qrtip = new QRTip();
+  return $qrtip->show();
 }
---
+++ src/QRTip.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace QRTip\WooCommerce;
+
+class QRTip
+{
+  public function __construct()
+  {
+
+  }
+
+  public function show(): string
+  {
+    return "QR Tip";
+  }
+}
